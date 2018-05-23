22e4ff27db76709b85e5b39f54d66baf0b408e1d
Steven Haring <sharing@ucdavis.edu>
2018-05-23 13:14:48 -0700

    added file structure, data, readme, initial script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cover_crops_almonds_statewide
+
+A repo for data and code related to plant surveys in almond orchards across the Central Valley treated with cover cropping programs.
---
+++ scripts/20180523_Spring18_CoverCropTeamMeeting.R
@@ -0,0 +1,19 @@
+# Workbook for figures for first cover crop team meeting (May 2018)
+# SCH May 23, 2018
+
+library(readr)
+WeedSurveys <- read_csv("data/20180518_CoverCropWeedSurveys.csv")
+str(WeedSurveys)
+
+# Ground cover ------------------------------------------------------------
+
+
+# Strip and alley weed cover ----------------------------------------------
+
+
+# Weed species diversity --------------------------------------------------
+
+
+# Cover crop establishment by species -------------------------------------
+
+
