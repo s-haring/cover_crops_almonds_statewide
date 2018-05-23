321895c592696b31422ea69273d04a9cd6045b42
Steven Haring <sharing@ucdavis.edu>
2018-05-23 16:20:30 -0700

    sorted some data, sketched some figures

--- scripts/20180523_Spring18_CoverCropTeamMeeting.R
+++ scripts/20180523_Spring18_CoverCropTeamMeeting.R
@@ -7,13 +7,39 @@ str(WeedSurveys)
 
 # Ground cover ------------------------------------------------------------
 
+# make a new column of percent ground cover
+library(tidyverse)
+PercentCover <- mutate(WeedSurveys, cover = Intercepts*2)
+# group weed species and cover crop species together - every observation is of crop, weed, or bare ground
+library(mosaic)
+PercentCover <- mutate(PercentCover, PlantType = case_when(
+  Species == "NONE" ~ "Bare",
+  Species == "SINAL" | Species == "VICVI" | Species == "LOLMU" | Species == "TRFRE" | Species == "RAPSR" | Species == "BRSNN" ~ "Cover",
+  TRUE ~ "Weed"))
+# sum the cover from each species into totals for the category
+CoverTotals <- PercentCover %>%
+  group_by(Year, Month, Day, Location, Rep, Treatment, TermTiming, Zone, PlantType) %>%
+  summarize(totalcover = sum(cover))
+# make the figure
+ggplot(data= CoverTotals, aes(x = PlantType, y = totalcover))
 
-# Strip and alley weed cover ----------------------------------------------
-
+? geom_boxplot()
 
 # Weed species diversity --------------------------------------------------
 
+# count species richness in each plot
+SpeciesCounts <- PercentCover %>%
+  filter(Species != "NONE") %>%
+  group_by(Location, Rep, Treatment, PlantType) %>%
+  summarise(richness = n_distinct(Species))
 
+#make the figure
+ggplot(data = SpeciesCounts, aes(x = Treatment, y = richness)) +
+  geom_() +
+  facet_wrap(~ Location) +
+  theme_bw()
+
+?geom_boxplot
 # Cover crop establishment by species -------------------------------------
 
-
+# use PercentCover dataframe for coverage of individual species
